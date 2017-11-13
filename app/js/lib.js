c828e2e0b5a269fff09da941ff0ad510adc46e2f
AnJSTA <Andrzej.Jedrzejewski@statravel.com>
2017-11-13 09:13:47 +0000

    more generic functions in lib.js

--- app/js/lib.js
+++ app/js/lib.js
@@ -1,11 +1,21 @@
-// currying function
-const curry = (fn) => {
-	return (a) => {
-		return (b) => {
-			return fn(a, b);
-		}
-	}
-};
+// currying function simple
+
+// const curry = (fn) => {
+// 	return (a) => {
+// 		return (b) => {
+// 			return fn(a, b);
+// 		}
+// 	}
+// };
+
+// curry function more generic
+const curry = (fn) => (
+	(...args) => (
+		fn.length <= 1 || args.length >= fn.length ? fn(...args) : args.reduce((acc, curr) => (
+			curry(acc.bind(null, curr)), fn)
+		)
+	)
+);
 
 // splitting data for more managable format
 const splitData = (data, chunk) => {
@@ -16,12 +26,19 @@ const splitData = (data, chunk) => {
 	}, []);
 };
 
-// composition function
-const compose = (f, g) => {
-	return (a) => {
-		return f(g(a));
-	}
-};
+// composition function simple 
+// const compose = (f, g) => {
+// 	return (a) => {
+// 		return f(g(a));
+// 	}
+// };
+
+// composition more generic
+const compose = (...fns) => (
+	(x) => fns.reduceRight(
+		(acc, curr) => curr(acc), x
+	)
+);
 
 
 // mapper accepts function than data
