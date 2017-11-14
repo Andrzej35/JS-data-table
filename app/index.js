39d63b8c270e7be19973ebb329e8fab6b28b8dc6
Andrzej35 <Andrzej35@users.noreply.github.com>
2017-11-14 16:59:54 +0000

    Update index.js
    The sort column function is with loops right now, and will be change to more functional approach soon

--- app/index.js
+++ app/index.js
@@ -2,9 +2,77 @@ import dataToDisplay from './js/data';
 import { curry, splitData, compose, mapper } from './js/lib';
 import { createHtml, createAttr, appendElement, createTrailingZeroes, createDateFormat } from './js/html-builds';
 
+// table search function 
+window.searchTable = function(e) {
+	
+		tbody.innerHTML = '';
+		pg.innerHTML ='';
+	
+		dataToDisplay.filter(function(x) {
+			
+			if((x.name.toUpperCase().indexOf(e.target.value.toUpperCase())) > -1) {
+				tbody.innerHTML += tbodyTmpl(x);
+			}
+	
+		});
+	};
+	
+
+// sorting columns 
+	window.sortTheColumn = function sortTheColumn(n) {
+
+		let rows, isSwitching, i, x, y, isSwitched, directions, switchCount = 0;
+		isSwitching = true;
+
+		directions = "asc"; 
+
+		while (isSwitching) {
+		  
+		  isSwitching = false;
+		  rows = tbody.getElementsByTagName("TR");
+		  
+		  for (i = 0; i < (rows.length - 1); i++) {
+			
+			isSwitched = false;
+			
+			x = rows[i].getElementsByTagName("TD")[n];
+			y = rows[i + 1].getElementsByTagName("TD")[n];
+			 
+			if (directions == "asc") {
+			  if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+				
+				isSwitched= true;
+				break;
+			  }
+			} else if (directions == "desc") {
+			  if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+				
+				isSwitched= true;
+				break;
+			  }
+			}
+		  }
+		  if (isSwitched) {
+			
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			isSwitching = true;
+			
+			switchCount ++; 
+		  } else {
+			
+			if (switchCount == 0 && directions == "asc") {
+			  directions = "desc";
+			  isSwitching = true;
+			}
+		  }
+		}
+	}; 
+
+	
 
 const container = document.getElementById('app');
-const template = (x) => `<th>${x}</th>`;
+
+const template = (x, i) => `<th onClick="sortTheColumn(${i})">${x}</th>`;
 const tbodyTmpl = (x) => `<tr><td id="${headersForTable()[0]}-column-${x.id}">${x.name}</td><td id="${headersForTable()[1]}-column-${x.id}">${createDateFormat(x.date).date}</td><td  id="${headersForTable()[2]}-column-${x.id}">${createDateFormat(x.date).hours}:${createDateFormat(x.date).minutes}${createDateFormat(x.date).ampm}</td><td id="${headersForTable()[3]}-column-${x.id}">${x.uploads}</td><td  id="${headersForTable()[4]}-column-${x.id}">${x.downloads}</td></tr>`;
 
 // limit data
@@ -16,6 +84,7 @@ const headersForTable = () => { return ['Name', 'Date', 'Time', 'Uploads', 'Down
 let pager = limit()[0];
 
 const data = curry(splitData)(dataToDisplay);
+console.log(data(pager));
 
 // ***********************
 const uldiv = createHtml('div');
@@ -32,15 +101,16 @@ createAttr(ul, 'class', ' hidelist ');
 const toggleClass = () => {
 	if(ul.classList.contains('activelist')) {
 		ul.classList.remove('activelist');
-		s[0].innerHTML = '▼';
+		s.innerHTML = '▼';
 	} else {
-		ul.classList += ' activelist ';
-		s[0].innerHTML = '▲';
+		ul.className += ' activelist ';
+		s.innerHTML = '▲';
 	}
 };
 
 ulspan.addEventListener('click', toggleClass);
 
+// display table
 window.displayPage = (e) => {
 	tbody.innerHTML = '';
 	pg.innerHTML ='';
@@ -83,7 +153,7 @@ window.renderPage = (e) => {
 	b[e.target.value].className = 'btn btn-primary btn-sm active ';
 };
 
-
+// pagination
 const pagination = (ar) => {
 	return ar.forEach((x, i) => {
 		pg.innerHTML += `<button class="btn btn-primary btn-sm" value="${i}" onclick="renderPage(event)">${i + 1}</button>`;
@@ -106,19 +176,7 @@ appendElement(tbody, tbl);
 appendElement(tbl, container);
 appendElement(pg, container);
 pagination(data(pager));
+
 b[0].className += ' active ';
 
 
-window.searchTable = function(e) {
-
-	tbody.innerHTML = '';
-	pg.innerHTML ='';
-
-	dataToDisplay.filter(function(x) {
-		
-		if((x.name.toUpperCase().indexOf(e.target.value.toUpperCase())) > -1) {
-			tbody.innerHTML += tbodyTmpl(x);
-		}
-
-	});
-};
